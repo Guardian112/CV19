6194b7c76623401932f5106509894d6a051a7581
kpc <kpc.kostroma@gmail.com>
2023-03-28 11:21:54 +0300

    Добавьте файлы проекта.

---
+++ 19/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CV19.ViewModel.Base
+{
+    internal abstract class VIewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        protected virtual bool Set<T>(ref T filed, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(filed, value)) return false;
+            filed = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+        public void Dispodse()
+        {
+            Dispose(true);
+        }
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+        }
+    }
+}
