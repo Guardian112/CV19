799a3c3f3638c8cd1471b9629872ba14f7a4a960
kpc <kpc.kostroma@gmail.com>
2023-03-29 11:35:01 +0300

    Save Point

--- 19/ViewModels/MainWindowViewModel.cs
+++ 19/ViewModels/MainWindowViewModel.cs
@@ -4,11 +4,22 @@ using System.Text;
 using System.Windows;
 using System.Windows.Input;
 using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
 namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region TestDataPoints : IEnumerable<DataPoint> - Тестовый набор данных для визуализации графиков
+
+        private IEnumerable<DataPoint> _TestDataPoints;
+
+        public IEnumerable<DataPoint> TestDataPoints { 
+            get => _TestDataPoints; 
+            set => Set(ref _TestDataPoints, value); 
+        }
+        #endregion
+
         #region Заголовок окна
         private string _Title = "Анализ статистики CV19";
         public string Title
@@ -52,6 +63,15 @@ namespace CV19.ViewModels
 
             #endregion
 
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for(var x =0d;x<=360;x+=0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+            TestDataPoints = data_points;
         }
-    }
+     }
 }
